469e465d1430c4dd980f946c4c9f7723b5b8b6d2
unb3rechenbar <tom.folgmann@icloud.com>
2023-06-08 13:14:33 +0200

    VelocityVerlet implementiert, Lösungen sehen noch seltsam aus, prüfe Systemgleichungen

--- Blatt 04 - Projekt 2 (Magnetpendel)/Simulation/src/header/Verfahren.hpp
+++ Blatt 04 - Projekt 2 (Magnetpendel)/Simulation/src/header/Verfahren.hpp
@@ -3,3 +3,12 @@ void leapfrogstep(double t, Lsng* L, dLsng (*F)(double,Lsng*)) {
     L->du += at.ddu * h;        // Geschwindigkeit zu t + h/2
     L->u += L->du * h;          // Position zu t + h
 }
+
+void verletstep(double t, Lsng* L, Lsng* pastL, dLsng (*F)(double,Lsng*)) {
+    Lsng tempL;       // Erzeuge temporäre Lösungsstruktur
+    tempL.u = L->u * 2 - pastL->u + F(t, L).ddu * h * h; // Berechne Position zu t + h
+    tempL.du = (tempL.u - pastL->u) / (2 * h);            // Berechne Geschwindigkeit zu t + h/2
+
+    *pastL = *L;    // Speichere alte Lösung
+    *L = tempL;     // Überschreibe alte Lösung mit neuer Lösung
+}
--- Blatt 04 - Projekt 2 (Magnetpendel)/Simulation/src/main.cpp
+++ Blatt 04 - Projekt 2 (Magnetpendel)/Simulation/src/main.cpp
@@ -100,8 +100,8 @@ typedef struct {
     Initialisiere die Lösung. Beachte dabei, daß die Ortskomponente zur Geschwindigkeitskomponente um h/2 verschoben ist. Die erste Iteration übernimmt für die Geschwindigkeit also Euler. 
 */
 Lsng L = {
-    .u = {1.0, 2.0},
-    .du = {3.0, 4.0}
+    .u = {0.0, 0.0},
+    .du = {0.0, 0.0}
 };
 
 /*
@@ -115,42 +115,72 @@ const double h = T/n;
 // Importiere Verfahren
 #include "header/Verfahren.hpp"
 
-
 /*
     Initialisiere die Systemgleichungen. Definiere zunächst Magnetanzahl und Orte.
 */
-const int s = 2; // Anzahl der Magnete
-const R2 Magnetorte[s] = {{0,0}, {1,1}}; // Orte der Magnete
+const int s = 3; // Anzahl der Magnete
+const R2 Magnetorte[s] = {{0.5,-0.5}, {0,0.5}, {-0.5,-0.5}}; // Orte der Magnete
 const double m = 1; // Masse des Pendels
 #include "header/Systemgleichungen.hpp"
 
 
 int main() {
-    printf("Hello World\n");
+    printf("--- Magnetpendelsimulation ---\n");
+
+    FILE* datei;
 
     /*
-        Löse die Systemgleichungen mithilfe von leapfrog und schreibe in die Datei
-        ../data/leapfrogsol.dat
+        Löse die Systemgleichungen mithilfe von leapfrog und schreibe in die Datei "../data/leapfrogsol.dat" (LeapFrog) oder "../data/velocityverletsol.dat" (Velocity Verlet). Wähle das Verfahren aus.
     */
-    FILE *datei = fopen("./data/leapfrogsol.dat", "w");
-    if (datei == NULL) {
-        printf("Datei konnte nicht geöffnet werden.\n");
-        return 1;
+    int Verfahren = 0;
+    printf("Wähle das Lösungsverfahren. Wähle (1) LeapFrog, (2) VelocityVerlet.\n");
+    scanf("%d", &Verfahren);
+
+    switch (Verfahren) {
+        case 1: {
+            datei = fopen("./data/leapfrogsol.dat", "w");
+
+            // leapfrog Verschiebung durch Euler initialisieren:
+            L.du += F(0, &L).ddu * h/2;
+
+            // Initiale Werte schreiben
+            fprintf(datei, "0\t%g\t%g\t\t\n", L.u.x, L.u.y);
+            fprintf(datei, "%g\t\t\t%g\t%g\n", h/2, L.du.x, L.du.y);
+            
+            for (int i = 1; i < n; i++) {
+                leapfrogstep(i*h, &L, F);
+                fprintf(datei, "%f\t%f\t%f\t\t\n", i*h, L.u.x, L.u.y);
+                fprintf(datei, "%f\t\t\t%f\t%f\n", i * h + h/2, L.du.x, L.du.y);
+            }
+            break;
+        }
+        case 2: {
+            datei = fopen("./data/velocityverletsol.dat", "w");
+
+            // Eulerstarter
+            Lsng pastL = L;
+            L.u = pastL.u + pastL.du * h/2;
+            L.du = pastL.du + F(0, &L).ddu * h/2;
+
+            // Gebe initiale Werte aus
+            fprintf(datei, "0\t%g\t%g\t%g\t%g\n", pastL.u.x, pastL.u.y, pastL.du.x, pastL.du.y);
+            fprintf(datei, "%g\t%g\t%g\t%g\t%g\n", h, L.u.x, L.u.y, L.du.x, L.du.y);
+
+            // Iteriere über die Zeitstützen
+            for (int i = 1; i < n; i++) {
+                verletstep(i*h, &L, &pastL, F);
+                fprintf(datei, "%g\t%g\t%g\t%g\t%g\n", i*h, L.u.x, L.u.y, L.du.x, L.du.y);
+            }
+            break;
+        }
+        default: {
+            printf("Verfahren nicht definiert.\n");
+            return 1;
+        }
     }
-
-    // leapfrog Verschiebung durch Euler initialisieren:
-    L.du += F(0, &L).ddu * h/2;
-
-    // Initiale Werte schreiben
-    fprintf(datei, "0\t%g\t%g\t\t\n", L.u.x, L.u.y);
-    fprintf(datei, "%g\t\t\t%g\t%g\n", h/2, L.du.x, L.du.y);
 
     // Lösung berechnen und in Datei schreiben
-    for (int i = 1; i < n; i++) {
-        leapfrogstep(i*h, &L, F);
-        fprintf(datei, "%f\t%f\t%f\t\t\n", i*h, L.u.x, L.u.y);
-        fprintf(datei, "%f\t\t\t%f\t%f\n", i * h + h/2, L.du.x, L.du.y);
-    }
+    
 
     fclose(datei);
 }
